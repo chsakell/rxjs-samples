87889d09bc40dda44919043f2f9ba83dcc20bd4e
chsakell <chsakell@gmail.com>
2017-03-02 22:44:54 +0200

    more transform-filter samples

--- src/app/transform-filter/transform-filter.component.ts
+++ src/app/transform-filter/transform-filter.component.ts
@@ -14,7 +14,18 @@ export class TransformFilterComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.bufferCount();
+        // this.takeUntil();
+        this.skipUntil();
+        // this.takeWhile();
+        // this.skipWhile();
+        // this.skip();
+        // this.take();
+        // this.findIndex();
+        // this.find();
+        // this.single();
+        // this.last();
+        // this.first();
+        // this.bufferCount();
         // this.buffer();
         // this.pluck();
         // this.mapTo();
@@ -23,9 +34,159 @@ export class TransformFilterComponent implements OnInit {
         // this.map();
     }
 
+    // skip items for a certain duration. then start emitting
+    skipUntil() {
+        const source$ = Observable.interval(1000);
+
+        source$
+            .skipUntil(Observable.timer(4000))
+            .subscribe(this.getSubscriber('skip-until'));
+    }
+
+    // take items for a certain duration. then stop
+    takeUntil() {
+        const source$ = Observable.interval(1000);
+
+        source$
+            .takeUntil(Observable.timer(4000))
+            .subscribe(this.getSubscriber('take-until'));
+    }
+
+    // skip items till a condintion is true
+    skipWhile() {
+        const source$ = Observable.range(1,10);
+
+        source$
+            .skipWhile(v => v < 5)
+            .subscribe(this.getSubscriber('skip-while'));
+    }
+
+    // take items till a condintion is true
+    takeWhile() {
+        const source$ = Observable.range(1,10);
+
+        source$
+            .takeWhile(v => v < 5)
+            .subscribe(this.getSubscriber('take-while'));
+    }
+
+    // skip the first # items
+    skip() {
+        const source$ = new Observable(observer => {
+            observer.next(1);
+            observer.next(2);
+            observer.next('hello'); 
+            observer.next('chris');
+            observer.next('christos')
+            observer.complete();
+        });
+
+        source$
+            .skip(3)
+            .subscribe(this.getSubscriber('skip'));
+    }
+
+    // take the first # items
+    take() {
+        const source$ = new Observable(observer => {
+            observer.next(1);
+            observer.next(2);
+            observer.next('hello'); 
+            observer.next('chris');
+            observer.next('christos')
+            observer.complete();
+        });
+
+        source$
+            .take(3)
+            .subscribe(this.getSubscriber('take'));
+    }
+
+    // return the index if found or -1
+    findIndex() {
+        const source$ = new Observable(observer => {
+            observer.next(1);
+            observer.next(2);
+            observer.next('hello'); // 2
+            observer.next('chris');
+            observer.next('christos')
+            observer.complete();
+        });
+
+        source$
+            .findIndex((x, i) => {
+                return JSON.stringify(x).indexOf('hello') > 0;
+            })
+            .subscribe(this.getSubscriber('find-index'));
+    }
+
+    // find (x,i) x: value, i: index
+    // returns undefine if not found
+    find() {
+        const source$ = new Observable(observer => {
+            observer.next(1);
+            observer.next(2);
+            observer.next('hello');
+            observer.next('chris');
+            observer.next('christos')
+            observer.complete();
+        });
+
+        source$
+            .find((x, i) => {
+                return JSON.stringify(x).indexOf('chr') > 0;
+            })
+            .subscribe(this.getSubscriber('find'));
+    }
+
+    // Get a single value. Only one value should emitted
+    single() {
+        const source$ = new Observable(observer => {
+            observer.next(1);
+
+            observer.complete();
+        });
+
+        source$
+            .single()
+            .subscribe(this.getSubscriber('single'));
+    }
+
+    // Get the last item emitted by an observable
+    // produces error if no value emitted
+    last() {
+        const source$ = new Observable(observer => {
+            observer.next(1);
+            observer.next(2);
+            observer.next('hello');
+            observer.next('chris');
+            observer.complete();
+        });
+
+        source$
+            .last()
+            .subscribe(this.getSubscriber('last'));
+    }
+
+    // Get the first item emitted by an observable
+    // produces error if no value emitted
+    first() {
+        const source$ = new Observable(observer => {
+            observer.next(1);
+            observer.next(2);
+            observer.next('hello');
+            observer.next('chris');
+            observer.complete();
+        });
+
+        source$
+            .first()
+            .subscribe(this.getSubscriber('first'));
+    }
+
     // buffer exact # of items before emit them
     bufferCount() {
-        Observable.range(1,15)
+        Observable.range(1, 15)
             .bufferCount(5)
             .subscribe(this.getSubscriber('buffer-count'));
     }
